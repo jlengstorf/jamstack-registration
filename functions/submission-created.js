9a9fe58f3a42cf8885e44895c6f91ca0e4dc0c3f
Jason Lengstorf <jason@lengstorf.com>
2020-04-06 11:23:50 -0700

    feat: write registrations to Fauna

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,33 @@
+//
+// process.env.FAUNA_API_SECRET
+
 exports.handler = async (event) => {
-  console.log(event);
+  const body = JSON.parse(event.body);
+  const { firstname, lastname, email } = body.payload.data;
+
+  const response = await fetch('https://graphql.fauna.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_API_SECRET}`,
+    },
+    body: {
+      query: `
+          mutation ($name: String! $email: String!) {
+            createRegistration(data: {
+              name: $name
+              email: $email
+            }) {
+              _id
+            }
+          }
+        `,
+      variables: { name: `${firstname} ${lastname}`, email },
+    },
+  })
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  console.log(response);
 
   return {
     statusCode: 200,
